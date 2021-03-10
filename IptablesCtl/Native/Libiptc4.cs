e6ab5a859ab3f1cae1f11573c317949b40789cdb
Styagkin Ilya <skywarer@mail.ru>
2021-03-10 08:51:21 +0300

    generalise lib name

--- IptablesCtl/Native/Libiptc4.cs
+++ IptablesCtl/Native/Libiptc4.cs
@@ -5,46 +5,46 @@ namespace IptablesCtl.Native
 {
     internal static class Libiptc4
     {
-        [DllImport("libip4tc.so")]
+        [DllImport("libip4tc")]
         public static extern IntPtr iptc_init(string tablename);      
         
-        [DllImport("libip4tc.so")]
+        [DllImport("libip4tc")]
         public static extern void iptc_free(IntPtr handle);
 
-        [DllImport("libip4tc.so")]
+        [DllImport("libip4tc")]
         public static extern IntPtr iptc_strerror(int err);
 
-        [DllImport("libip4tc.so", CharSet = CharSet.Ansi)]
+        [DllImport("libip4tc", CharSet = CharSet.Ansi)]
         public static extern IntPtr iptc_first_chain(IntPtr handle);
 
-        [DllImport("libip4tc.so", CharSet = CharSet.Ansi)]
+        [DllImport("libip4tc", CharSet = CharSet.Ansi)]
         public static extern IntPtr iptc_next_chain(IntPtr handle);
         
-        [DllImport("libip4tc.so", CharSet = CharSet.Ansi)]
+        [DllImport("libio4tc", CharSet = CharSet.Ansi)]
         public static extern IntPtr iptc_read_counter([MarshalAs(UnmanagedType.LPStr, SizeConst = 32)] string label, uint num, IntPtr handle);        
 
-        [DllImport("libip4tc.so")]
+        [DllImport("libio4tc")]
         public static extern int iptc_builtin(string chain, IntPtr handle);
 
-        [DllImport("libip4tc.so")]
+        [DllImport("libio4tc")]
         public static extern IntPtr iptc_first_rule(string chain, IntPtr handle);
 
-        [DllImport("libip4tc.so")]
+        [DllImport("libio4tc")]
         public static extern IntPtr iptc_next_rule(IntPtr prev, IntPtr handle);
 
-        [DllImport("libip4tc.so")]
+        [DllImport("libio4tc")]
         public static extern IntPtr iptc_get_target(IntPtr entry, IntPtr handle);
 
-        [DllImport("libip4tc.so", SetLastError = true)]
+        [DllImport("libio4tc", SetLastError = true)]
         public static extern int iptc_commit(IntPtr handle);
 
-        [DllImport("libip4tc.so", SetLastError = true)]
+        [DllImport("libio4tc", SetLastError = true)]
         public static extern int iptc_append_entry([MarshalAs(UnmanagedType.LPStr, SizeConst = 32)] string label, IntPtr entry, IntPtr handle);
         
-        [DllImport("libip4tc.so", SetLastError = true)]
+        [DllImport("libio4tc", SetLastError = true)]
         public static extern int iptc_replace_entry([MarshalAs(UnmanagedType.LPStr, SizeConst = 32)] string label, IntPtr entry, uint num, IntPtr handle);
         
-        [DllImport("libip4tc.so", SetLastError = true)]
+        [DllImport("libio4tc", SetLastError = true)]
         public static extern int iptc_delete_num_entry([MarshalAs(UnmanagedType.LPStr, SizeConst = 32)] string label, uint num, IntPtr handle);
 
     }
